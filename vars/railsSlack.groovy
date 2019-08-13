b35ed6025e11de977b2c216f3ac8720b8e9f686d
Charlie Pitkin <charlie@wearefine.com>
2019-08-13 15:06:46 +0300

    Added license header, Updated copyright year
    Each file needed a license header
    The copyright needed updated
    
    Signed-off-by: Charlie Pitkin <charlie@wearefine.com>

--- vars/railsSlack.groovy
+++ vars/railsSlack.groovy
@@ -1,3 +1,8 @@
+// Copyright (c) 2019 FINE Design Group, Inc.
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
 #!/usr/bin/env groovy
 import hudson.tasks.test.AbstractTestResultAction;
 
